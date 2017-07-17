1f3a8810d65b9f7c2e76db06a1c69256bad58caf
Dzung <dung.0924789022@gmail.com>
2017-07-17 14:25:22 +0700

    easier access

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,12 +7,9 @@ import UserDetailContainer from './containers/UserDetailContainer';
 
 const routes = (
     <Route path="/" component={Layout}>
-        <IndexRoute component={UserListContainer} />
-        <Route path="users">
-            <IndexRoute component={UserListContainer} />
-            <Route path="search" component={UserSearchContainer} />
-            <Route path=":id" component={UserDetailContainer} />
-        </Route>
+        <IndexRoute component={UserSearchContainer} />
+        <Route path="users/:id" component={UserDetailContainer} />
+        
     </Route>
 );
 
