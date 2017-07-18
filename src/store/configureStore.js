8f4b26c2cef5fc75d1a78afe52a5c16d5b1c881c
Dzung <dung.0924789022@gmail.com>
2017-07-18 09:34:42 +0700

    a

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createLogger from 'redux-logger';
 import createSaga from 'redux-saga';
 import reducer from '../reducers/reducers';
 import rootSaga from '../sagas/sagas';
